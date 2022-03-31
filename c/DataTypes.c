e04e37fad8a8e2c0062287a95af8958166998a9c
Luigi Selmi <luigiselmi@gmail.com>
2022-03-31 15:04:27 +0200

    added a recursive function

--- c/DataTypes.c
+++ c/DataTypes.c
@@ -4,6 +4,7 @@
 int atoi(char s[]);
 int lower(int c);
 int squeeze(char s[], int c);
+void printd(int n);
 
 /* Implements some functions to show the C data types properties */
 
@@ -19,6 +20,8 @@ int main() {
   printf("Removes \'c\' from \"chaccacah\": %s\n", t);
   printf("New size = %d\n", num_chars_left);
   printf("Shifted \'a\': %d %d\n", 'a', 'a' >> 1);
+  printf("Print a number as chars: -12345: %c\n");
+  printd(-12345);
 }
 
 /* Converts an integer represented as a string (char array) into a number */
@@ -27,6 +30,18 @@ int atoi(char s[]) {
   for (int i = 0; s[i] > '0' && s[i] <= '9'; ++i)
     n = 10 * n + s[i] - '0';
   return n;
+}
+/* Converts the digits of a number in characters. It is based on recursion.
+   The characters are written in the standard output.
+*/
+void printd(int n) {
+  if (n < 0) {
+    putchar('-');
+    n = -n;
+  }
+  if (n / 10)
+    printd(n / 10);  // recursive call
+  putchar(n % 10 + '0');
 }
 
 /* Converts a char into its lower case representation */
