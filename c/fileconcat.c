fcdb4f88a6ac1c44d819f70260dac702d3fab960
Luigi Selmi <luigiselmi@gmail.com>
2022-04-09 15:52:41 +0200

    write to stderr

--- c/fileconcat.c
+++ c/fileconcat.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h> // exit()
 
 /* Similarly to the unix command cat, prints
    on the standard output the file whose names
@@ -7,21 +8,27 @@
 int main(int argc, char *argv[]) {
    FILE *fp;  // pointer to a file
    void filecopy(FILE *source, FILE *target);
+   char *prog = argv[0];
 
    if (argc == 1) // no files to open
       filecopy(stdin, stdout); // stdin and stdout are defined in <stdio.h>
    else
       while (--argc > 0)
          if ((fp = fopen(*++argv, "r")) == NULL) {
-            printf("fileconcat: the file %s cannot be opened.\n", *argv);
-            return 1;
+            fprintf(stderr, "fileconcat: the file \'%s\' cannot be opened.\n", *argv);
+            exit(1);
          }
          else {
             filecopy(fp, stdout);
             fclose(fp);
          }
 
-   return 0;
+   if (ferror(stdout)) {
+      fprintf(stderr, "%s: error writing on stdout.\n", prog);
+      exit(2);
+   }
+
+   exit(0);
 }
 void filecopy(FILE *source, FILE *target) {
    int c;
