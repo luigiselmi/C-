58fee956eb7f3e3e6e06afbf1689ea1145fe5e58
Luigi Selmi <luigiselmi@gmail.com>
2022-04-13 16:04:16 +0200

    removed code dec to bin

--- c/sp/binary2decimal.c
+++ c/sp/binary2decimal.c
@@ -28,29 +28,21 @@
    float sum = 1.0;
    for (int n = 23; n > 0; ++n)
     sum += bit[n] * 2^(-n);
-
-   What we are more interested in is how to represent a decimal value
-   as floating point with 32 binary digits (single precision). The steps
-   are as follows
-   1) put the number in normalized scientific notation, e.g., if the number
-      is 10 digits divide by 10^9
-   2) transform the digit on the left of the decimal point into binary form
-   3) transform the digits on the right of the decimal point into binary form
-
 */
 int char2bin(char c[], int i[], int length);
 int dec_exp(int ie[], int len);
 double dec_mant(int im[], int len);
 int power(int base, int n);
 int dec2bin_exp(float num);
-void bin_exp(int dec_exp, char bin[], int len);
-void bin_mant(double dec_mant, char bin[], int len);
+void bin_exp(int dec_exp, int bin[], int len);
+void bin_mant(double dec_mant, int bin[], int len);
+int dec2bin_mant(int im[], int len);
 
 int main () {
-  // binary to decimal
+
   char exponent[MAX_DIGITS_EXPONENT];
   char mantissa[MAX_DIGITS_MANTISSA];
-  /*
+
   printf("Write the binary representation of the exponent\n");
   scanf("%s", exponent);
   printf("Exponent: %s\n", exponent);
@@ -83,37 +75,7 @@ int main () {
 
   double value_decimal = mant_decimal * power(2, exp_decimal);
   printf("Value (dec): %.10f\n", value_decimal);
-  */
-  // decimal to binary
-  float decimal;
-  printf("Write the rational number in decimal\n");
-  scanf("%f", &decimal);
-  int exp1;
-  float ratio1 = frexp(decimal, &exp1);
-  printf("Number (decimal): %.10f, Ratio %f, Exp.: %d\n", decimal, ratio1, exp1);
 
-  double int2;
-  double dec2 = modf(decimal, &int2);
-  printf("Number (decimal): %.10f, Int.: %.0f, Dec. %f, \n", decimal, int2, dec2);
-  // exponent
-  int exp2 = dec2bin_exp(decimal);
-  printf("Number (decimal): %.10f, Exponent %d\n", decimal, exp2);
-
-  char bin[MAX_DIGITS_EXPONENT];
-  for (int i = 0; i < MAX_DIGITS_EXPONENT; i++)
-    bin[i] = 0;
-
-  bin_exp(exp2, bin, MAX_DIGITS_EXPONENT);
-  for (int i = 0; i < MAX_DIGITS_EXPONENT; i++)
-    printf("%c", bin[i]);
-  printf("\n");
-
-  // mantissa
-  char mant_array[MAX_DIGITS_MANTISSA];
-  for (int i = 0; i < MAX_DIGITS_MANTISSA; i++)
-    mant_array[i] = 0;
-
-  bin_mant(ratio1, mant_array, MAX_DIGITS_MANTISSA);
 }
 /* Transforms the input array of char into an array of binary digits */
 int char2bin(char c[], int i[], int length) {
@@ -149,34 +111,4 @@ int power(int base, int n) {
   for (p = 1; n - 1 >= 0; --n)
     p *= base;
   return p;
-}
-/* Computes the exponent to be used to convert
-   a decimal number into a binary in floating
-   point.
-*/
-int dec2bin_exp(float num) {
-  int exp;
-  float dec = frexp(num, &exp);
-  return BIAS + exp - 1;
-}
-/* Converts a decimal number into a binary one.
-   The remainder of a division by 2 is always '1' or '0'.
-*/
-void bin_exp(int dec_exp, char bin[], int len) {
-  int remainder = 0;
-  int i = 1;
-  while (dec_exp != 0) {
-    remainder = dec_exp % 2;
-    if (remainder == 0)
-      bin[len - i] = '0';
-    else
-      bin[len - i] = '1';
-    i++;
-    dec_exp /= 2;
-  }
-  bin[i] = '\0';
-}
-/* Computes the mantissa in binary format of a number in decimal */
-void bin_mant(double dec_mant, char bin[], int len) {
-  printf("Mantissa (dec): %f\n", dec_mant);
 }
