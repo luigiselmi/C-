88b99d46c1231cb4c401eaa5272f83a5c8992d59
Luigi Selmi <luigiselmi@gmail.com>
2022-04-15 16:13:19 +0200

    basic c++

--- c++/Hello.cpp
+++ c++/Hello.cpp
@@ -1,6 +1,40 @@
-#include<iostream>
+#include <iostream>
+using namespace std;
+
+#define MAX_ATTEMPTS 3
+
+void print_sqr(double x);
+double square(double x);
+bool accept();
 
 int main()
 {
-  std::cout<<"Hello, world! \n";
+  cout<<"Hello, world! \n";
+  auto x = 4.0; // type defined by its init value
+  print_sqr(x);
+  const double sqr = square(x); // constant evaluated at runtime
+  if (accept())
+    print_sqr(sqr);
+}
+double square(double x) {
+  return x * x;
+}
+void print_sqr(double x) {
+  cout << "The square of " << x << " is " << square(x) << "\n";
+}
+bool accept() {
+  int attempts = 0;
+  char answer = 0;
+  while (attempts < MAX_ATTEMPTS) {
+    cout << "Do you want to proceed (y/n) ?\n";
+    cin >> answer;
+    if (answer == 'y')
+      return true;
+    else if (answer == 'n')
+      return false;
+    else
+      cout << "Use only \'y\' or \'n\'.\n";
+    ++attempts;
+  }
+  return false; // in case of wrong answer after MAX_ATTEMPTS
 }
--- c++/Square.cpp
+++ c++/Square.cpp
@@ -1,58 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-double square(double x)
-{
-  return x*x;
-}
+const int v1[10] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-void print_square(double x)
-{
-  cout << "the square of " << x << " is " << square(x) << "\n";
-}
-
-void copy_fct()
-{
-  int v1[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int v2[10];
-  for (int i = 0; i < 10; i++)
-    v2[i] = v1[i];
-
-  for (int x: v2)
-    cout << x << "\n";
-}
-
-struct Vector {
-  int size;
-  double* elem;
-};
-
-void vector_init(Vector& v, int s)
-{
-  v.elem = new double[s];
-  v.size = s;
-}
-
-double read_and_sum(int s)
-{
-  Vector v;
-  vector_init(v, s);
-  for (int i = 0; i < s; i++)
-    cin >> v.elem[i];  // read elements from standard input
-  double sum = 0.0;
-  for (int i = 0; i < s; i++)
-    sum += v.elem[i];
-  return sum;
-}
-
-bool accept()
-{
-  cout << "Do you want to proceed (y or n)?\n";
-  char answer = 0;
-  cin >> answer;
-  if (answer == 'y') return true;
-  return false;
-}
+void print_square(double x);
+void copy_fct();
+double read_and_sum(int s);
+bool accept();
 
 int main()
 {
@@ -63,4 +17,51 @@ int main()
     double sum = read_and_sum(5);
     cout << sum;
   }
+}
+
+double square(double x) {
+  return x * x;
+}
+
+void print_square(double x) {
+  cout << "the square of " << x << " is " << square(x) << "\n";
+}
+/* Copies an array's values into another */
+void copy_fct() {
+
+  int v2[10];
+  for (int i = 0; i < 10; i++)
+    v2[i] = v1[i];
+
+  for (int x: v2) // range-for loop
+    cout << x << "\n";
+}
+
+struct Vector {
+  int size;
+  double* elem;
+};
+
+void vector_init(Vector& v, int s) {
+  v.elem = new double[s];
+  v.size = s;
+}
+
+double read_and_sum(int s) {
+  Vector v;
+  vector_init(v, s);
+  for (int i = 0; i < s; i++)
+    cin >> v.elem[i];  // read elements from standard input
+  double sum = 0.0;
+  for (int i = 0; i < s; i++)
+    sum += v.elem[i];
+  return sum;
+}
+
+bool accept() {
+  cout << "Do you want to proceed (y or n)?\n";
+  char answer = 0;
+  cin >> answer;
+  if (answer == 'y') return true;
+  return false;
 }
