8b78a7e0fb1830d9188367029de28571bf82ab62
Luigi Selmi <luigiselmi@gmail.com>
2022-05-18 17:32:28 +0200

    linear interpolation (least squares method)

--- c/sp/interpolation.c
+++ c/sp/interpolation.c
@@ -1,6 +1,6 @@
 /*
   Computes the linear interpolation from data
-  read on a file using the least squares method.
+  read from a file using the least squares method.
   Compile using the command:
 
   $ cc interpolation.c parse_data_points.c -o interpolation
@@ -8,16 +8,15 @@
 */
 #include <stdio.h>
 #include <stdlib.h> // exit()
-#include "parse_data_points.h" // declare the functions used to read
-                               // the input file and to create an array of points
-
-
-int parse(FILE *source, struct point data[]);
-FILE *_open(int argc, char *argv[]);
+#include <math.h> // sqrt()
+#include "parse_data_points.h" // declare the functions _open() and
+                               // parse() used to read the input file
+                               // and to create an array of points
 
 int main(int argc, char *argv[]) {
    FILE *fp;  // pointer to a file
    int N; // number of data points
+   const double sigma = 0.01; // measurement error (or variance)
 
    if (argc == 1) { // no files to open
       printf("The input data file is missing.");
@@ -26,14 +25,35 @@ int main(int argc, char *argv[]) {
    else
      fp = _open(argc, argv);
 
-   fscanf(fp, "%d", &N);  // reads the number of lines
+   fscanf(fp, "%d", &N);
    struct point data[N];
    parse(fp, data);
-
-   for (int i = 0; i < N; i++)
-      printf("%d) x = %f, y = %f\n", i, data[i].x, data[i].y);
-
    fclose(fp);
+
+   double S, Sx, Sy;
+
+   double s2 = sigma * sigma;
+   for (int i = 0; i < N; i++) {
+     Sx += data[i].x  / s2;
+     Sy += data[i].y  / s2;
+     S += 1. / s2;
+   }
+
+   double Stt, t[N], A, B, sigma_A, sigma_B;
+   for (int i = 0; i < N ; i++) {
+     t[i] = (data[i].x - Sx / S) / sigma;
+     Stt += t[i] * t[i];
+     B += t[i] * data[i].y / sigma;
+   }
+
+   B /= Stt;
+   A = (Sy - Sx * B) / S;
+
+   sigma_A = sqrt ((1. + Sx * Sx / (S * Stt)) / S);
+   sigma_B = sqrt (1./ Stt);
+
+   printf("A = %.2f, sigma_A = %.2f\n", A, sigma_A);
+   printf("B = %.2f, sigma_B = %.2f\n", B, sigma_B);
 
    exit(0);
 }
