fa7c6a3582aff1033b951f2742a87976cbfaf981
Luigi Selmi <luigiselmi@gmail.com>
2022-06-05 18:36:33 +0200

    full description of the 2d random walk

--- c/sp/random_walk_2d.c
+++ c/sp/random_walk_2d.c
@@ -12,9 +12,66 @@
 	and writes the values on a file. The average values of x^2 at step t,
 	<x^2(t)> represents the (squared) probability for the traveler to
 	be at position x in the square lattice.
-	The data can be plotted using gnuplot and the command:
+	The <x^2(t)> data can be plotted using gnuplot and the command:
 
-	gnuplot> plot 'rw1d_ave.dat'
+	gnuplot> plot 'rw2d_ave.dat'
+
+  From the plot it can be seen that <x^2(t)> is linear according to the
+	theory that says
+
+	<x^2(t)> = a^2 * t / D
+
+  where a is the length of the edges between two nodes of the square
+	lattice (in our case a = 1), t is the step (or time), and D is the
+	dimension (in our case D = 2). The above equation has been determined
+	from the probability distribution of traveler position in the square
+	lattice as a function of the position x = (x1, x2) and the step (time)
+
+	P(x, t) = (D / 2*Pi*t)^(D/2) * exp(-Dx^2 / 2*t*a^2 )
+
+	so that <x^2(t)> = S(x^2 * P(x, t)dx)  (S represents the integration operator)
+
+  The application also stores in another file, rw2d_prob.dat, the position
+	(x1, x2) at each step of any random walk (sample) that is a power of 10
+	between 1 and 5, that is 10, 100, 1000, 10000 and 100000. We can plot the
+	distribution of the positions achieved by the random traveler after, for
+	instance 100 steps, in all the random walks. The position reached after
+	100 steps will be distributed around a value of (100/2)^(1/2), that is 7.07.
+	We can filter the data from the file to consider only the position after 100
+	steps
+
+	$ grep "T 99 " rw2d_prob.dat > rw2d_prob_100.dat
+
+	If we use gnuplot to plot the data we set the range for x1 and x2
+
+	gnuplot> set xrange [-300:300]
+  gnuplot> set yrange [-300:300]
+
+	then we plot the filtered data
+
+	gnuplot> plot 'rw2d_prob_100.dat' using 6:8 linecolor 'blue'
+
+  if we repeat the experiment filtering the data to extract the position
+	after, let's say, 10000 step we will see the the distribution is much
+	larger and the points are more dispersed.
+
+	Another experiment is to compute the number of times a certain position
+	in the square lattice has been reached by the random traveler after a
+	certain number of steps. For instance, we can count the number of times
+	the traveler went through the point at (0, 0) after 100 steps
+
+	$ grep "T 99 X1 0 X2 0" rw2d_prob.dat |wc -l
+	59
+
+	If we repeat the test for the position (10, 10)
+
+	$ grep "T 99 X1 10 X2 10" rw2d_prob.dat |wc -l
+  4
+
+	we see that it's much less likely. We can compute the values for all the
+	points in the square lattice, within the ranges we have set above, to have
+	the probability distribution for t = 100 or for any other value of t for
+	which we have the data.  
 
 	Credit: Barone et al. - Scientific Programming, https://chimera.roma1.infn.it/SPENG/
 */
@@ -44,13 +101,13 @@ int main(void) {
   double *x2Average, *x2Error;
   RANDOM_TYPE a, seed, randomNumber;
 
-  fDat = fopen("rw2d_ave.dat","w");
+  fDat = fopen("rw2d_ave.dat","w");  // <x^2(t)>
   if (fDat == NULL) {
     printf("Error: can't open the file to store the average values\n");
     exit(MY_EXIT_FAILURE);
   }
 
-  fProb = fopen("rw2d_prob.dat","w");
+  fProb = fopen("rw2d_prob.dat","w"); // (x1, x2) at t = {10, 100, 1000, ..}
   if (fProb == NULL) {
     printf("Error: can't open the file to store the probabilities'\n");
     exit(MY_EXIT_FAILURE);
@@ -91,7 +148,7 @@ int main(void) {
     exit(MY_EXIT_FAILURE);
   }
 
-  /* determiniamo quante potenze di dieci possiamo conservare */
+  /* let's find out which power of 10 of the number of step we can store */
   if(numberOfSteps < 10){
     printf("Error: numberOfSteps is too small for this analysis\n");
     exit(MY_EXIT_FAILURE);
@@ -115,7 +172,7 @@ int main(void) {
 
   randomNumber = seed;
 
-  for (j = 0; j < numberOfSamples; j++) { // each sample is a complete random walk
+  for (j = 0; j < numberOfSamples; j++) { // each sample is a complete random walk of a numberOfSteps
     position.x1 = 0;
     position.x2 = 0;
     k = 0;
@@ -139,7 +196,7 @@ int main(void) {
       x4 = x2 * x2;
       x2Average[i] += x2;
       x2Error[i] += x4;
-      if (i == allPowers[k]) {
+      if (i == allPowers[k]) { // if step is a power of k we store the position (x1, x2)
 	      k++;
 	      fprintf(fProb, "S %ld T %ld X1 %ld X2 %ld\n", j, i, position.x1, position.x2);
       }
